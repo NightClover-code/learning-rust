8a759f1481271aff6f20b15f2bb31305aba71530
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-10-17 17:37:10 +0100

    learnt about println! function

---
+++ src/print.rs
@@ -0,0 +1,16 @@
+pub fn run(){
+  //print to console
+  println!("Hello from print.rs!");
+  
+  //formatting
+  println!("{} is from {}", "Achraf", "Morocco");
+
+  //positional arguments
+  println!("{0} is from {1} and {0} likes to {2}", "Achraf", "Morocco", "code");
+
+  //named arguments
+  println!("{name} likes to play {activity}", name = "John", activity="baseball");
+
+  //placeholder traits
+  println!("Binary: {:b} Hex: {:x} Octal: {:o}", 10, 10, 10);
+}
