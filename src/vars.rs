3494ce02f3f1b16eb78069ce0a39825343215e4a
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-10-17 17:46:30 +0100

    learnt about variables

---
+++ src/vars.rs
@@ -0,0 +1,18 @@
+pub fn run(){
+  //mutable / immutable vars
+  let name = "Achraf";
+  let mut age = 54;
+
+  println!("My name is {} and I'm {}.", name, age);
+  age = 65;
+  println!("My name is {} and I'm {}.", name, age);
+
+  //constants
+  const ID: i32 = 001;
+  println!("ID: {}", ID);
+
+  //multiple vars
+  let (my_name, my_age) = ("Achraf", 20);
+
+  println!("My name is {} and I'm {}.", my_name, my_age);
+}
