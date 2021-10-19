321808a14f91349687763e15e6001826eaaf1f1f
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-10-19 16:27:44 +0100

    learnt about types

--- src/types.rs
+++ src/types.rs
@@ -8,5 +8,23 @@
 */
 
 pub fn run() {
-    println!("Helko");
+    //i32 int
+    let x = 1;
+
+    //f64 int
+    let f = 2.55;
+
+    //print max size
+    println!("Max i32: {}", std::i32::MAX);
+    println!("Max i64: {}", std::i64::MAX);
+
+    //boolean
+    let is_active = true;
+    let is_greater = 10 < 5;
+
+    //characters
+    let a1 = 'a';
+    let face = '\u{1F600}';
+
+    println!("{:?}", (x, f, is_active, is_greater, a1, face));
 }
