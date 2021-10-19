1608db7090bb382d491dafd9e05bfd73a57c2e17
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-10-19 15:41:26 +0100

    better code formatting

--- src/print.rs
+++ src/print.rs
@@ -1,16 +1,23 @@
-pub fn run(){
-  //print to console
-  println!("Hello from print.rs!");
-  
-  //formatting
-  println!("{} is from {}", "Achraf", "Morocco");
+pub fn run() {
+    //print to console
+    println!("Hello from print.rs!");
 
-  //positional arguments
-  println!("{0} is from {1} and {0} likes to {2}", "Achraf", "Morocco", "code");
+    //formatting
+    println!("{} is from {}", "Achraf", "Morocco");
 
-  //named arguments
-  println!("{name} likes to play {activity}", name = "John", activity="baseball");
+    //positional arguments
+    println!(
+        "{0} is from {1} and {0} likes to {2}",
+        "Achraf", "Morocco", "code"
+    );
 
-  //placeholder traits
-  println!("Binary: {:b} Hex: {:x} Octal: {:o}", 10, 10, 10);
+    //named arguments
+    println!(
+        "{name} likes to play {activity}",
+        name = "John",
+        activity = "baseball"
+    );
+
+    //placeholder traits
+    println!("Binary: {:b} Hex: {:x} Octal: {:o}", 10, 10, 10);
 }
