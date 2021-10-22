7b94ed7557fe0199d73a34cac3b6bcf6c3f22817
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-10-23 00:30:13 +0100

    learnt about arrays, cant add to them

--- src/arrays.rs
+++ src/arrays.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 pub fn run() {
     let mut nums: [i32; 5] = [2, 6, 4, 6, 5];
 
@@ -7,5 +9,11 @@ pub fn run() {
 
     println!("Single Value: {}", nums[0]);
 
-    println!("Array length: {}", nums.len())
+    println!("Array length: {}", nums.len());
+
+    println!("Array occupies {} bytes", mem::size_of_val(&nums));
+
+    let slice: &[i32] = &nums[2..];
+
+    println!("Slice: {:?}", slice);
 }
