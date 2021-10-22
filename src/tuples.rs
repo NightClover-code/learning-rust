4ead6d9b709f3b7b37b0aaa1711f74ec6e977bf8
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-10-23 00:15:02 +0100

    learnt about tuples

--- src/tuples.rs
+++ src/tuples.rs
@@ -1,1 +1,8 @@
-pub fn run() {}
+pub fn run() {
+    let person: (&str, &str, i8) = ("Achraf", "Morocco", 37);
+
+    println!(
+        "{} is from {} and is {} years old.",
+        person.0, person.1, person.2
+    );
+}
